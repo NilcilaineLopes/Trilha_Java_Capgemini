d396f16a82a2e7b85ebb9c37eeb9db96b16a831f
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-16 11:28:42 -0300

    Cria primeiro componente

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/primeiro-componente/primeiro-component.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/primeiro-componente/primeiro-component.component.ts
@@ -1,3 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-primeiro-componente",
+  template: "<h2>Primeiro Componente",
+  styles: ["h2  {color: red; }"]
+})
 
 
 export class PrimeiroComponenteComponent{}
