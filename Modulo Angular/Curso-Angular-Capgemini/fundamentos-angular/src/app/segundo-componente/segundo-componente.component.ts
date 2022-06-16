8e1eacf9d397aed65d83dea69eae945f7dafdd7a
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-16 11:41:13 -0300

    Cria segundo componente via terminal

---
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo-componente',
+  templateUrl: './segundo-componente.component.html',
+  styleUrls: ['./segundo-componente.component.css']
+})
+export class SegundoComponenteComponent {
+
+}
