f4665ec3262a87bff0b7d203daabaa91e4feb8df
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-16 17:00:02 -0300

    Inseri o component pipi-costumizado

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/app.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-component.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicaPorPipe } from './multiplica-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
-    SegundoComponenteComponent
+    SegundoComponenteComponent,
+    MultiplicaPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
