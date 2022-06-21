f8cf8a570cfe4e7d6a3c37f6daa0b9902345eb53
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-21 12:42:59 -0300

    Cria componente pai e filho para fazer cominicação entre si

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/app.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
+import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
+import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,9 @@ import { ComponentePersonalizadoComponent } from './componente-personalizado/com
     PipeCustomizadoComponent,
     TwoWayDataBindingComponent,
     RenderizandoListasComponent,
-    ComponentePersonalizadoComponent
+    ComponentePersonalizadoComponent,
+    ComponentePaiComponent,
+    ComponenteFilhoComponent
   ],
   imports: [
     BrowserModule,
---
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/componente-pai/componente-pai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-pai',
+  templateUrl: './componente-pai.component.html',
+  styleUrls: ['./componente-pai.component.css']
+})
+export class ComponentePaiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
