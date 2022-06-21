3e2fec308a220ca216c8f2f73033aadeded0e765
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-21 13:42:53 -0300

    Adiciona o sobrenome do elemento pai e implemeta o metodo da classe pai

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/componente-filho/componente-filho.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/componente-filho/componente-filho.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-componente-filho',
   templateUrl: './componente-filho.component.html',
   styleUrls: ['./componente-filho.component.css']
 })
-export class ComponenteFilhoComponent implements OnInit {
+export class ComponenteFilhoComponent {
+  @Input() sobrenome = "";
+  @Output() mostraNome = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  nome = "";
 }
