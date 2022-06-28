56c45f70a2252901b0c4ebba469ffba53e22d6a5
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-06-28 14:09:46 -0300

    Insere propriedade de texto e implementa o OnChanges

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/ciclo-de-vida/ciclo-de-vida.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/ciclo-de-vida/ciclo-de-vida.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-ciclo-de-vida',
   templateUrl: './ciclo-de-vida.component.html',
   styleUrls: ['./ciclo-de-vida.component.css']
 })
-export class CicloDeVidaComponent implements OnInit {
+export class CicloDeVidaComponent implements OnInit, OnChanges {
+  horario =  new Date();
+  timer: any = null!;
+
+  @Input() texto = "";
 
   ngOnInit(): void {
+    console.log("O evento OnInit disparou");
+    this.timer = setInterval(() => this.horario = new Date(), 1000);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 }
