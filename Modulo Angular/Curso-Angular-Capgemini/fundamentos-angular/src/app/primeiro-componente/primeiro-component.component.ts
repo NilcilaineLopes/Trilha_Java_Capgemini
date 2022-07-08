1ab8c60cebf68ceb4bad90eb42e590866082d690
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-08 14:57:06 -0300

    Adiciona espaço

--- Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/primeiro-componente/primeiro-component.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/fundamentos-angular/src/app/primeiro-componente/primeiro-component.component.ts
@@ -3,8 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-primeiro-componente",
   template: "<h2>Primeiro Componente",
-  styles: ["h2  {color: red; }"]
+  styles: ["h2  { color: red; }"]
 })
-
 
 export class PrimeiroComponenteComponent{}
