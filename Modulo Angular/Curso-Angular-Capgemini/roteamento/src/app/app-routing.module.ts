b382e2dcf86d3b253a1de4199da5a3a6689929b7
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-18 18:58:32 -0300

    Remove a importação do CommonModule

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
   ]
 })
 export class AppRoutingModule { }
