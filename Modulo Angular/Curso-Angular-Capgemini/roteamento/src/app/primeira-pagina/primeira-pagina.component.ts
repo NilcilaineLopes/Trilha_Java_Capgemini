2341504c5364952f787435af8fdb95d9b58965da
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-19 12:32:45 -0300

    Adiciona navegação para a segunda pagina

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/primeira-pagina/primeira-pagina.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/primeira-pagina/primeira-pagina.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-primeira-pagina',
   templateUrl: './primeira-pagina.component.html',
   styleUrls: ['./primeira-pagina.component.css']
 })
-export class PrimeiraPaginaComponent implements OnInit {
+export class PrimeiraPaginaComponent {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
-  }
+    moverParaSegundaPagina() {
+      this.router.navigate(["segunda-pagina"]);
+    }
 
 }
