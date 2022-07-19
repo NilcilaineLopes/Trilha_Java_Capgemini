ee7d07d3788c6dbe440b61ccf6cfdc89b0cd42ab
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-19 11:42:44 -0300

    Adiciona componente ao modulo

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiraPaginaComponent
   ],
   imports: [
     BrowserModule,
