2b961c9038056f26a6142f8394203cd321173ac2
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-26 12:09:11 -0300

    update

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/pagina-protegida/pagina-protegida.component.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/pagina-protegida/pagina-protegida.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-pagina-protegida',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaProtegidaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate([""]);
   }
 
 }
