9382fe562f89f17efae16c2bf1d607dede1fca46
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-25 21:10:47 -0300

    Adiciona path

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 const routes: Routes = [
   {path: "primeira-pagina", component: PrimeiraPaginaComponent },
-  {path: "segunda-pagina", component: SegundaPaginaComponent }
+  {path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-paramentros/:id", component: PaginaComParamentrosComponent },
 ]
 
 @NgModule({
