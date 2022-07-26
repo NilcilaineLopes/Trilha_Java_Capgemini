6f099334c6c003799a30d05f10fa4feb49504dec
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-25 23:09:29 -0300

    Adiciona path e canActivate

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 const routes: Routes = [
-  {path: "primeira-pagina", component: PrimeiraPaginaComponent },
-  {path: "segunda-pagina", component: SegundaPaginaComponent },
-  {path: "pagina-com-paramentros/:id", component: PaginaComParamentrosComponent },
-  {path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-paramentros/:id", component: PaginaComParamentrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent},
 ]
 
 @NgModule({
