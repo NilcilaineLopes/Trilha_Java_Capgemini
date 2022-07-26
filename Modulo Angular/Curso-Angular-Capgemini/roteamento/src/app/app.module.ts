a4f271f45ac1dc791d700e5777726f51b774da05
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-25 23:15:35 -0300

    Adiciona FormsModule

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
     SegundaPaginaComponent,
-    PaginaComParamentrosComponent
+    PaginaComParamentrosComponent,
+    PaginaProtegidaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
