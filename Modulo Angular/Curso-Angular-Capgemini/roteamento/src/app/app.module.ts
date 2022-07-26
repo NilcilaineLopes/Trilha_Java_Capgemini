0509a6d0f30ada04ddf657ebdfba8e4bffa7b66e
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-25 21:09:30 -0300

    Adiciona component

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaComParamentrosComponent } from './pagina-com-paramentros/pagina-com-paramentros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaComParamentrosComponent
   ],
   imports: [
     BrowserModule,
