8cb95aa861bd01329b4eccdd945d61b09f38c226
NilcilaineLopes <nilcilopes2015@gmail.com>
2022-07-26 11:13:41 -0300

    Faz importação e implementa verificação de login

--- Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/auth.guard.ts
+++ Modulo Angular/Curso-Angular-Capgemini/roteamento/src/app/auth.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+    ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    if (!this.auth.estaAutenticado()){
+      this.router.navigate(["login"]);
+      return false;
+    }
+
+    return true;
   }
 
 }
